be3e82ec90b77f94b255133c71870d27dc8095b4
FrostShock <46273569+FrostShock@users.noreply.github.com>
2021-10-06 22:31:00 +0200

    Add files via upload

--- SuperSpells.lua
+++ SuperSpells.lua
@@ -1,3 +1,17 @@
+function ShootWand()
+  CastSpellByName("Shoot")
+end
+
+function Resurrection(spell)
+  SendChatMessage("Resurrecting %t !")
+  CastSpellByName(spell)
+end
+
+function Summon()
+  SendChatMessage("Summoning %t - please click portal!")
+  CastSpellByName("Ritual of Summoning")
+end
+
 function Polymorph()
   SpellStopCasting()
   CastSpellByName("Polymorph")
@@ -8,8 +22,25 @@ function Blink()
   CastSpellByName("Blink")
 end
 
--- Key Bindings
-BINDING_HEADER_SUPERSPELLS = "Super Spells"
+function Portal(city)
+--  echo(city);
+--  DEFAULT_CHAT_FRAME:AddMessage("|cffffff00City: "..city);
+  SendChatMessage("Making portal to "..city.." !")
+  CastSpellByName("Portal: "..city)
+end
 
+-- Key Bindings
+BINDING_HEADER_SUPERSPELLS = "SuperSpells"
+
+BINDING_NAME_WAND = "Shoot Wand"
+BINDING_NAME_RESURRECTION = "Resurrection (Priest)"
+BINDING_NAME_REDEMPTION = "Redemption (Paladin)"
+BINDING_NAME_SUMMON = "Summon (Warlock)"
 BINDING_NAME_POLYMORPH = "Polymorph"
 BINDING_NAME_BLINK = "Blink"
+BINDING_NAME_PORTALSW = "Portal: Stormwind"
+BINDING_NAME_PORTALIF = "Portal: Ironforge"
+BINDING_NAME_PORTALDA = "Portal: Darnassus"
+BINDING_NAME_PORTALOG = "Portal: Orgrimmar"
+BINDING_NAME_PORTALUC = "Portal: Undercity"
+BINDING_NAME_PORTALTB = "Portal: Thunder Bluff"
