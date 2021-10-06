3c68a8523e690a07f35765c6aa7bb9907abe1ddc
FrostShock <46273569+FrostShock@users.noreply.github.com>
2021-10-06 21:05:21 +0200

    Add files via upload

---
+++ SuperSpells.lua
@@ -0,0 +1,15 @@
+function Polymorph()
+  SpellStopCasting()
+  CastSpellByName("Polymorph")
+end
+
+function Blink()
+  SpellStopCasting()
+  CastSpellByName("Blink")
+end
+
+-- Key Bindings
+BINDING_HEADER_SUPERSPELLS = "Super Spells"
+
+BINDING_NAME_POLYMORPH = "Polymorph"
+BINDING_NAME_BLINK = "Blink"
